4a92f4d39311c8f3b3e7cc0334c6737050bbea64
mikerukwaro <102186288+mikerukwaro@users.noreply.github.com>
2022-07-01 14:57:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,129 +1,73 @@
-from selenium.webdriver.common.by import By
-from xlsxwriter import Workbook
-from selenium import webdriver
 import datetime
+from bs4 import BeautifulSoup
+import requests
+from xlsxwriter import Workbook
 import time
-import re
 
 
-def clean_text_content(inner_body):
-    inner_body_less_tnr = inner_body.strip("\t\n \r")
-    non_ascci_removed = re.sub(r"[^\x00-\x7F]+", " ", inner_body_less_tnr)
-    extra_space_removed = re.sub(" +", " ", non_ascci_removed)
-    body_single_newline = re.sub(r"\n\s*\n", "\n\n", extra_space_removed)
-    url_stripped_content = re.sub(r"http\S+", "", body_single_newline)
-    return url_stripped_content
-
-
-class OnlyProfessors:
+class youtube_link:
     def __init__(self):
-        self.driver = None
-        self.sitemap_to_search = None
-        #self.homepage_link = None
-        self.sitemaps = None
-        self.account_name = "assignmentbay"
-        self.minimum_characters = 10
-        self.scrap_complete = False
-        self.discarded = 0
+        self.minimum_characters = 1
+        self.account_name = "bundlescrape"
 
-    def initialize_defaults(self):
-        self.driver = webdriver.Chrome(
-            "assets/chromedriver.exe"
-        )
-
-    def homepage(self):
-        self.homepage_link = "https://assignmentbay.org/sitemap_index.xml"
-        self.driver.get(self.homepage_link)
 
     def driver_setup(self):
-        self.driver.maximize_window()
-        time.sleep(4)
+        url = "https://coreyms.com/"
+        source = requests.get(url).text
+        self.soup = BeautifulSoup(source, "lxml")
+        self.article_search()
 
-    def sitemap(self):
-        all_sitemaps = self.driver.find_elements(
-            By.XPATH, '//*[@id="sitemap"]/tbody/tr/td/a'
-        )
-        sitemap_urls = [map_.get_attribute("href") for map_ in all_sitemaps]
-        for url in sitemap_urls[:2]:
-            self.driver.get(url)
-            self.articles(url)
+    def article_search(self):
+        articles = self.soup.find_all("article", itemtype="https://schema.org/CreativeWork")
+        content_lists = []
+        for article in articles:
+            headline = article.find("a", class_= "entry-title-link").text
+            headline = headline.split(",")
+            headline_string_ ="".join(headline)
+            print(headline_string_)
 
-    def articles(self, url):
-        all_articles = self.driver.find_elements(
-            By.XPATH, '//*[@id="sitemap"]/tbody/tr/td/a'
-        )
-        articles_urls = [art_.get_attribute("href") for art_ in all_articles]
+            content = article.find("div", class_= "entry-content").text
+            for cont_ in content:
+                cont_.split(",")
+                #print(content)
+            try:
+                infame = article.find("iframe")["src"]
+                split1 = infame.split("/")[4]
+                video_id = split1.split("?")[0]
+            except:
+                pass
 
-        articles_to_save = []
-        for article in articles_urls[:20]:
-            self.driver.get(article)
-            with open("last.txt", "w+") as f:
-                f.write(url)
-            with open("last.txt", "a+") as f:
-                f.write("\n" + article)
-            question_details = self.get_question_title_content()
-            if question_details[-1] is not None:
-                articles_to_save.append(question_details)
+            yt_link = f"https//:youtube.com/watch?v={video_id}"
+            yt_link = yt_link.split(",")
+            yt_link_string_ = "".join(yt_link)#convering list to string
+            print(yt_link_string_)
 
-        chunked_list = self.create_chunks(articles_to_save, 10)
-        for chunk in chunked_list:
-            self.excel_saver(chunk)
-            time.sleep(5)
+            art_tuple = (headline_string_, yt_link_string_, content)
+            content_lists.append(art_tuple)
 
-    def create_chunks(self, raw_list, chunk_size):
-        chucked_list = []
+        self.excel_saver(content_lists)
 
-        for i in range(0, len(raw_list), chunk_size):
-            chucked_list.append(raw_list[i : i + chunk_size])
-
-        return chucked_list
-
-    def get_question_title_content(self):
-        title_raw = self.driver.find_element(By.CLASS_NAME, "entry-header")
-        title = title_raw.text
-        content = self.get_article_content()
-        clean_content = clean_text_content(content)
-        return title, clean_content
-
-    def get_article_content(self):
-        try:
-            content_raw = self.driver.find_element(
-                By.XPATH, '//div[@class="entry-content"]'
-            )
-            return content_raw.text
-        except:
-            return None
-
-    def excel_saver(self, get_question_title_content):  # will take a list of tuples
-        link_name = (
-            str(datetime.datetime.now())
-            .replace(" ", "_")
-            .replace(":", "_")
-            .split(".")[0]
-        )
+    def excel_saver(self, content_lists):  # will take a list of tuples
+        link_name = (str(datetime.datetime.now()).replace(" ", "_").replace(":", "_").split(".")[0])
         with Workbook(f"{self.account_name}_{link_name}.xlsx") as workbook:
             worksheet = workbook.add_worksheet()
             row, col = 0, 0
 
-            for question in get_question_title_content:
-                title = question[0]
-                content = question[-1]
+            for marks in content_lists:
+                headline_string_ = marks[0]
+                yt_link_string_ = marks[1]
+                content = marks[2]
                 try:
-                    if len(content) > self.minimum_characters:
-                        worksheet.write(row, col, title)
-                        worksheet.write(row, col + 1, content)
-                        row += 1
-
+                    worksheet.write(row, col, headline_string_)
+                    worksheet.write(row, col + 1, yt_link_string_)
+                    worksheet.write(row, col + 2, content)
+                    row += 1
                 except Exception as e:
                     print(e, "inner save while loop")
 
     def run(self):
-        self.initialize_defaults()
-        self.homepage()
         self.driver_setup()
-        self.sitemap()
 
-
-if __name__ == "__main__":
-    scrapper = OnlyProfessors()
+if __name__== "__main__":
+    scrapper = youtube_link()
     scrapper.run()
